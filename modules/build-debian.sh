337282f423644d650242d821c0bf4aedb8e6e680
Michael Clausen <encodeering@gmail.com>
2018-01-06 17:50:58 +0100

    adds an alpine image for php 7.2 fpm
    * alpine doesn't define a variant for apache as for now

---
+++ modules/build-debian.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# configuration
+#   env.ARCH
+#   env.PROJECT
+#   env.VERSION
+#   env.VARIANT
+#   env.REPOSITORY
+
+set -e
+
+import com.encodeering.docker.config
+import com.encodeering.docker.docker
+
+case "$VERSION" in
+    7.2) FROM=stretch; TO=stretch-slim ;;
+      *) FROM=jessie ; TO=jessie       ;;
+esac
+
+docker-pull "$REPOSITORY/debian-$ARCH:$FROM" "debian:$TO"
+
+docker build -t "$DOCKER_IMAGE" "$PROJECT/$VERSION/$FROM/$VARIANT"
