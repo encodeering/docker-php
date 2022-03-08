831e381abc961b4aa9c51f03e30c7b4680c5df4f
Michael Clausen <encodeering@gmail.com>
2022-03-08 12:02:47 +0100

    updates buildpack to the latest version
    * https://github.com/docker-library/php/commit/b3e617367771db15eeafabc6a52209c0b24ea823

--- modules/build-alpine.sh
+++ modules/build-alpine.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/alpine-$ARCH:3.10" "alpine:3.10"
+docker-pull "$REPOSITORY/alpine-$ARCH:3.15" "alpine:3.15"
 
-docker-build "$PROJECT/$VERSION/alpine3.10/$VARIANT"
+docker-build "$PROJECT/$VERSION/alpine3.15/$VARIANT"
