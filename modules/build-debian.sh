f92e984c06fee26aa2182fa17ab56e7feb0261d5
Michael Clausen <encodeering@gmail.com>
2024-09-14 13:56:28 +0200

    updates php to the latest version
    * https://github.com/docker-library/php/commit/97f95ebb364610948f8db0815516f7ac302499f6

--- modules/build-debian.sh
+++ modules/build-debian.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/debian-$ARCH:bullseye-slim" "debian:bullseye-slim"
+docker-pull "$REPOSITORY/debian-$ARCH:bookworm-slim" "debian:bookworm-slim"
 
-docker-build "$PROJECT/$VERSION/bullseye/$VARIANT"
+docker-build "$PROJECT/$VERSION/bookworm/$VARIANT"
